13ec2773736b7897df3cd8cb02a832736b30556a
Mostafa Elhemali <mostafa.elhemaly@gmail.com>
2015-11-06 14:54:06 -0800

    Symptom chooser spinner

--- src/com/mostybie/symtrack/MyActivity.java
+++ src/com/mostybie/symtrack/MyActivity.java
@@ -2,14 +2,37 @@ package com.mostybie.symtrack;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.*;
 
 public class MyActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private List<String> _allSymptoms;
+
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		initializeSymptomList();
+		initializeSymptomChooser();
+	}
+
+	/**
+	 * Initializes the symptom chooser spinner from the symptom list.
+	 */
+	private void initializeSymptomChooser() {
+		Spinner symptomChooser = ((Spinner)findViewById(R.id.symptomChooser));
+		ArrayAdapter<String> symptomChooserAdapter =
+				new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, _allSymptoms);
+		symptomChooser.setAdapter(symptomChooserAdapter);
+	}
+
+	private void initializeSymptomList() {
+		// TODO: Actual symptom list logic
+		_allSymptoms = Arrays.asList("Sad", "Lonely");
+	}
 }
