4fada2a94df10f67f4db38d8ff807d8944f354b8
Mostafa Elhemali <mostafa.elhemaly@gmail.com>
2015-11-07 14:48:25 -0800

    Finish and Cancel tracking buttons

--- src/com/mostybie/symtrack/MyActivity.java
+++ src/com/mostybie/symtrack/MyActivity.java
@@ -3,12 +3,11 @@ package com.mostybie.symtrack;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.*;
 
 import java.util.*;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity implements AdapterView.OnItemSelectedListener {
 	private List<String> _allSymptoms;
 	private List<SymptomRecord> _records; // TODO: Persist those
 
@@ -31,10 +30,23 @@ public class MyActivity extends Activity {
 		ArrayAdapter<String> symptomChooserAdapter =
 				new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, _allSymptoms);
 		getSymptomChooser().setAdapter(symptomChooserAdapter);
+		getSymptomChooser().setOnItemSelectedListener(this);
 	}
 
 	private Spinner getSymptomChooser() {
 		return (Spinner)findViewById(R.id.symptomChooser);
+	}
+
+	private Button getExperienceForThirtyMinutesButton() {
+		return (Button)findViewById(R.id.trackForShortPeriodButton);
+	}
+
+	private Button getCancelTrackingButton() {
+		return (Button)findViewById(R.id.cancelTrackingButton);
+	}
+
+	private Button getFinishTrackingButton() {
+		return (Button)findViewById(R.id.finishTrackingButton);
 	}
 
 	private void initializeSymptomList() {
@@ -42,8 +54,72 @@ public class MyActivity extends Activity {
 		_allSymptoms = Arrays.asList("Sad", "Lonely");
 	}
 
+	private String getSelectedSymptom() {
+		return (String)getSymptomChooser().getSelectedItem();
+	}
+
 	public void experienceForThirtyMinutes(View experienceButton) {
-		String symptom = (String)getSymptomChooser().getSelectedItem();
-		_records.add(SymptomRecord.fromNowForXMinutes(symptom, 30));
+		String selectedSymptom = getSelectedSymptom();
+		_records.add(SymptomRecord.fromNowForXMinutes(selectedSymptom, 30));
+		adjustButtonVisibility(selectedSymptom);
+	}
+
+	public void cancelTracking(View cancelTrackingButton) {
+		String selectedSymptom = getSelectedSymptom();
+		SymptomRecord currentRecord = getCurrentlyTrackingRecordForSymptom(selectedSymptom);
+		if (currentRecord != null) {
+			_records.remove(currentRecord);
+			adjustButtonVisibility(selectedSymptom);
+		}
+	}
+
+	public void finishTracking(View finishTrackingButton) {
+		String selectedSymptom = getSelectedSymptom();
+		SymptomRecord currentRecord = getCurrentlyTrackingRecordForSymptom(selectedSymptom);
+		if (currentRecord != null) {
+			currentRecord.setEndToNow();
+			adjustButtonVisibility(selectedSymptom);
+		}
+	}
+
+	private SymptomRecord getCurrentlyTrackingRecordForSymptom(String symptom) {
+		Date now = Calendar.getInstance().getTime();
+		for (SymptomRecord record : _records) {
+			if (record.getSymptom().equalsIgnoreCase(symptom) &&
+					record.isTimeInWindow(now)) {
+				return record;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		String selectedSymptom = (String)parent.getItemAtPosition(position);
+		adjustButtonVisibility(selectedSymptom);
+	}
+
+	private void adjustButtonVisibility(String selectedSymptom) {
+		SymptomRecord currentRecord = getCurrentlyTrackingRecordForSymptom(selectedSymptom);
+		if (currentRecord == null) {
+			// Not currently tracking this symptom
+			getExperienceForThirtyMinutesButton().setVisibility(View.VISIBLE);
+			getCancelTrackingButton().setVisibility(View.INVISIBLE);
+			getFinishTrackingButton().setVisibility(View.INVISIBLE);
+		} else {
+			// Currently tracking this symptom
+			getExperienceForThirtyMinutesButton().setVisibility(View.INVISIBLE);
+			getCancelTrackingButton().setVisibility(View.VISIBLE);
+			if (currentRecord.didJustStart()) {
+				getFinishTrackingButton().setVisibility(View.INVISIBLE);
+			} else {
+				getFinishTrackingButton().setVisibility(View.VISIBLE);
+			}
+		}
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> parent) {
+		// Should never happen...
 	}
 }
--- src/com/mostybie/symtrack/SymptomRecord.java
+++ src/com/mostybie/symtrack/SymptomRecord.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class SymptomRecord {
 	private final String _symptom;
 	private final Date _startTime;
-	private final Date _endTime;
+	private Date _endTime;
 
 	public SymptomRecord(String symptom, Date startTime, Date endTime) {
 		_symptom = symptom;
@@ -24,10 +24,33 @@ public class SymptomRecord {
 		return new SymptomRecord(symptom, startTime, endTime);
 	}
 
-	@SuppressWarnings("unused")
 	public String getSymptom() { return _symptom; }
 	@SuppressWarnings("unused")
 	public Date getStartTime() { return _startTime; }
 	@SuppressWarnings("unused")
 	public Date getEndTime() { return _endTime; }
+
+	public void setEndToNow() {
+		_endTime = Calendar.getInstance().getTime();
+	}
+
+	public boolean isTimeInWindow(Date time) {
+		return _startTime.compareTo(time) <= 0 &&
+				_endTime.compareTo(time) > 0;
+	}
+
+	/**
+	 * Checks if tracking for this symptom has JUST started (i.e. within the last minute).
+	 * @return true iff just started. False if started a long time ago or hasn't started.
+	 */
+	public boolean didJustStart() {
+		Calendar calendar = Calendar.getInstance();
+		Date now = calendar.getTime();
+		calendar.setTime(_startTime);
+		Date windowStart = calendar.getTime();
+		calendar.add(GregorianCalendar.MINUTE, 1);
+		Date windowEnd = calendar.getTime();
+		return windowStart.compareTo(now) <= 0 &&
+				windowEnd.compareTo(now) > 0;
+	}
 }
