5bfbe2759f9e6ca5dfdb836d1f6111904175ed22
Mostafa Elhemali <mostafa.elhemaly@gmail.com>
2015-12-06 13:10:46 -0800

    Show a button to track previous days

--- src/com/mostybie/symtrack/MainActivity.java
+++ src/com/mostybie/symtrack/MainActivity.java
@@ -1,11 +1,15 @@
 package com.mostybie.symtrack;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import java.util.*;
 
@@ -51,5 +55,35 @@ public class MainActivity extends Activity {
 		newAnswerIntent.putExtra(AnswerQuestionActivity.QUESTION_POSITION, 0);
 		newAnswerIntent.putExtra(AnswerQuestionActivity.DAY_IN_QUESTION, _today);
 		startActivity(newAnswerIntent);
+	}
+
+	public static class PreviousDayPickerFragment extends DialogFragment
+		implements DatePickerDialog.OnDateSetListener {
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Get yesterday's year/month/day for default/max value
+			final Calendar c = Calendar.getInstance();
+			c.add(Calendar.DAY_OF_MONTH, -1);
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+
+			DatePickerDialog dialog = new DatePickerDialog(getActivity(), this, year, month, day);
+			dialog.getDatePicker().setMaxDate(c.getTime().getTime());
+			return dialog;
+		}
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+			Intent newAnswerIntent = new Intent(getActivity(), AnswerQuestionActivity.class);
+			newAnswerIntent.putExtra(AnswerQuestionActivity.QUESTION_POSITION, 0);
+			newAnswerIntent.putExtra(AnswerQuestionActivity.DAY_IN_QUESTION,
+					new DayDate(year, monthOfYear, dayOfMonth));
+			startActivity(newAnswerIntent);
+		}
+	}
+
+	public void trackPreviousDay(View trackPreviousDayButton) {
+		new PreviousDayPickerFragment().show(getFragmentManager(), "previousDayPicker");
 	}
 }
